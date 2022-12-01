28690ecd53fb46ce392fbaef512b3ab8d7694f27
Fabian Meyer <fameyer@brita.net>
2022-12-01 12:50:27 +0100

    Added command line arguments for choosing which day to run

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,30 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Control.Monad
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
 import qualified Calories as Calories
 
+data Options = Options
+  { day :: Int }
+
 main :: IO ()
-main = do
-    day1Result <- Calories.runInput <$> T.readFile "input/Day1.txt"
-    forM_ day1Result (putStrLn . T.unpack) 
+main = runDay =<< execParser opts
+  where
+    opts = info (sample <**> helper)
+      ( fullDesc
+     <> progDesc "Run solutions of Advent of Code 2022"
+     <> header "Advent of Code 2022" )
+
+sample :: Parser Options
+sample = Options <$> argument auto (metavar "DAY" <> help "Day to run" )
+
+runDay :: Options -> IO ()
+runDay (Options 1) = run Calories.runInput "input/Day1.txt"
+runDay _ = return ()
+
+run :: (T.Text -> [T.Text]) -> FilePath -> IO ()
+run action file = do
+    result <- action <$> T.readFile file
+    forM_ result (putStrLn . T.unpack) 
