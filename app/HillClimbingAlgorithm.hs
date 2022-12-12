84c29022e328e37fdac68e3a7f0fe9da9078b244
Fabian Meyer <fameyer@brita.net>
2022-12-12 21:42:03 +0100

    Day 12

--- app/HillClimbingAlgorithm.hs
+++ app/HillClimbingAlgorithm.hs
@@ -8,111 +8,103 @@ import qualified Text.Parsec as P
 import qualified Text.Parsec.Text as P
 import qualified Text.Parsec.Char as P
 import Text.Parsec ((<|>))
-import Data.Maybe (catMaybes)
-import PrettyPrint
+import Data.Maybe (mapMaybe)
+import PrettyPrint (printKeyValuePairs)
 import Debug.Trace (trace)
+import qualified Data.Char as C
+import qualified Data.Graph.Inductive.Graph as G
+import qualified Data.Graph.Inductive.PatriciaTree as G
+import qualified Data.Graph.Inductive.Query.SP as G
+import qualified Data.Graph.Inductive.Query.BFS as G
 
-data Command = Noop | AddX Int deriving (Show)
+type Elevation = Int
+data ElevationKind = Regular | Start | End deriving Show
 
-data CommandQueueEntry = CommandQueueEntry Int Command deriving (Show)
+type NodeData = (Int, Int, Elevation, ElevationKind) 
 
 data Result = Result {
-    sumInterestedCycles :: Int,
-    image :: String
+    pt1 :: Int,
+    pt2 :: Int
 } 
-
-data State = State [CommandQueueEntry] Int
 
 instance Show Result where    
     show (Result {..}) = printKeyValuePairs [
-        ("Sum Interested Cycles" :: T.Text, show sumInterestedCycles),
-        ("Image" :: T.Text, "\n" <> image)]
+        ("Shortest Path from Start to End" :: T.Text, show pt1),
+        ("Shortest Path from any 'a' to End" :: T.Text, show pt2)]
 
 -- Entry point
 runInput :: T.Text -> Result
 runInput input = solve $ parseInput input
 
 -- Logic
-solve :: [Command] -> Result
-solve commands = Result (sum interestedCycles) image
-    where commandQueueEntries = mkCommandQueueEntry <$> commands
-          initial = State commandQueueEntries 1
-          steps = zip [1..] $ run initial
-          xs = (\(cycle, (State _ x)) -> (cycle, x)) <$> steps
-          interestedCycles = (\(cycle, x) -> cycle * x) . head <$> (L.chunksOf 40 $ drop 19 xs)
-          image = unlines $ map draw <$> (L.chunksOf 40 xs)
+solve :: G.Gr NodeData Int -> Result
+solve graph = Result (solvePt1 graph) (solvePt2 graph)
+          
+solvePt1 :: G.Gr NodeData Int -> Int
+solvePt1 graph = L.length shortestPath - 1
+        where nodes = G.labNodes graph
+              (startNode,_) = head $ filter isStart nodes
+              (endNode, _) = head $ filter isEnd nodes
+              (Just shortestPath) = G.sp startNode endNode graph
 
-draw :: (Int, Int) -> Char
-draw (c, x) | abs (((c - 1) `mod` 40) - x) < 2 = '#'
-draw _ = '.'
+solvePt2 :: G.Gr NodeData Int -> Int
+solvePt2 graph = L.length shortestPath - 1
+        where nodes = G.labNodes graph
+              (endNode, _) = head $ filter isEnd nodes
+              inverseEdges = inverseEdge <$> G.labEdges graph
+              (graph' :: G.Gr NodeData Int) = G.mkGraph nodes inverseEdges
+              paths = G.bft endNode graph'
+              shortestPath = head $ filter (isPathToA graph) paths
 
-mkCommandQueueEntry :: Command -> CommandQueueEntry
-mkCommandQueueEntry Noop = CommandQueueEntry 1 Noop
-mkCommandQueueEntry cmd@(AddX _) = CommandQueueEntry 2 cmd
+isPathToA :: G.Gr NodeData Int -> G.Path -> Bool 
+isPathToA graph (last : _) = case G.lab graph last of 
+    Just (_, _, 0, _) -> True
+    _ -> False
+                
+inverseEdge :: G.LEdge Int -> G.LEdge Int
+inverseEdge (s, d, l) = (d, s, l)
 
-run :: State -> [State]
-run s@(State ((CommandQueueEntry 1 Noop) : tail) x) = s : run (State tail x)
-run s@(State ((CommandQueueEntry 1 (AddX dx)) : tail) x) = s : run (State tail (x + dx))
-run s@(State ((CommandQueueEntry n ce) : tail) x) = s : run (State (CommandQueueEntry (n - 1) ce : tail) x)
-run (State [] _) = []
+isStart :: G.LNode NodeData -> Bool
+isStart (node, (_, _,_, Start)) = True
+isStart _ = False
 
+isEnd :: G.LNode NodeData  -> Bool
+isEnd (node, (_, _,_, End)) = True
+isEnd _ = False
+    
 -- Parsing
-parseInput :: T.Text -> [Command]
+parseInput :: T.Text -> G.Gr NodeData Int
 parseInput lines = case eitherCommands of 
             Right commands -> commands
             Left err -> error $ show err
-    where parser = (P.sepEndBy1 parseCommand P.endOfLine)
+    where parser = do 
+            input <- (P.sepEndBy1 (P.many1 parseElevation) P.endOfLine)
+            let nodes = toNodes input
+            let edges = toEdges nodes <> toEdges (L.transpose nodes)
+            return $ G.mkGraph (concat nodes) edges
           (eitherCommands) = P.parse parser "input" lines
 
-parseCommand :: P.Parser Command  
-parseCommand = parseNoop <|> parseAddX
+toNodes :: [[(Elevation, ElevationKind)]] -> [[G.LNode NodeData]]
+toNodes input = do 
+        (y, row) <- zip [0..] input
+        return $ do 
+            (x, (elevation, elevationKind)) <- zip [0..] row
+            return $ toLNode y x (length row) elevation elevationKind
 
-parseNoop :: P.Parser Command  
-parseNoop = P.string "noop" *> return Noop
+toEdges :: [[G.LNode NodeData]] -> [G.LEdge Int]
+toEdges nodes = do 
+    nodeRow <- nodes
+    ((lid, (_, _, el, _)), (rid, (_, _, er, _))) <- zip nodeRow (tail nodeRow) 
+    let lToR = if er - el <= 1 then [(lid,rid, 1)] else []
+    let lToL = if el - er <= 1 then [(rid, lid, 1)] else []
+    lToR <> lToL
 
-parseAddX :: P.Parser Command  
-parseAddX = do 
-    P.string "addx" *> P.space 
-    prefix <- P.option "" $ P.string "-"
-    number <- P.many1 P.digit
-    return . AddX . read $ prefix <> number
+toLNode :: Int -> Int -> Int -> Elevation -> ElevationKind -> G.LNode NodeData
+toLNode y x xs elevation elevationKind = (y * xs + x, (x, y, elevation, elevationKind))
 
-
--- parseCommand :: P.Parser Command
--- parseCommand = P.string "$" *> P.space *> (parseChangeDirectory <|> parseListContent)
-    
--- parseChangeDirectory ::  P.Parser Command
--- parseChangeDirectory = do
---         P.string "cd" <* P.space
---         path <- parsePath
---         return $ ChangeDirectory path
-
--- parsePath :: P.Parser Path
--- parsePath = parseParent <|> parseRoot <|> parseSubDirectory
-
--- parseParent :: P.Parser Path
--- parseParent = P.string ".." *> return Parent
-
--- parseRoot :: P.Parser Path
--- parseRoot = P.string "/" *> return Root
-
--- parseSubDirectory :: P.Parser Path
--- parseSubDirectory = SubDirectory <$> parseName
-
--- parseListContent ::  P.Parser Command
--- parseListContent = P.string "ls" *> pure ListContent
-        
--- parseDirectoryContent :: P.Parser DirectoryContent
--- parseDirectoryContent = parseDirectory <|> parseFile
-
--- parseDirectory :: P.Parser DirectoryContent
--- parseDirectory = Directory <$> (P.string "dir" *> P.space *> parseName)
-
--- parseFile :: P.Parser DirectoryContent
--- parseFile = do
---     size <- read <$> P.many1 P.digit <* P.space
---     name <- parseName
---     return $ File name size
-
--- parseName :: P.Parser T.Text
--- parseName = T.pack <$> P.many1 (P.satisfy (/= '\n'))
+parseElevation :: P.Parser (Elevation, ElevationKind)
+parseElevation = inspect <$> P.letter 
+    where inspect 'S' = (toElevation 'a', Start)
+          inspect 'E' = (toElevation 'z', End)
+          inspect it = (toElevation it, Regular)
+          toElevation it = C.ord it - C.ord 'a'
